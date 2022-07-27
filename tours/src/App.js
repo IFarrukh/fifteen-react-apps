53e711f99d80300ea1a26ed112da5051e8f2af8b
Farrukh <fa55ukh@gmail.com>
2022-07-26 19:49:40 -0700

    finish tours app

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import Loading from './Loading';
-import Tours from './Tours';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
 
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-	return <h2>Tours Project Setup</h2>;
+	const [loading, setLoading] = useState(false);
+	const [tour, setTour] = useState([]);
+
+	const removeTour = (id) => {
+		const newTour = tour.filter((t) => t.id !== id);
+
+		setTour(newTour);
+	};
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const res = await fetch(url);
+			const tours = await res.json();
+			setLoading(false);
+			setTour(tours);
+		} catch (error) {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	} else {
+		return (
+			<main>
+				<Tours tours={tour} removeTour={removeTour} />
+			</main>
+		);
+	}
 }
 
 export default App;
